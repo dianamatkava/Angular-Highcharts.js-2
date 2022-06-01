6f431b1164bd9ea3e483df0f2d0654a6f7fc1680
dianaGera <dianamatkava@gmail.com>
2022-06-01 19:23:42 +0200

    create new xharts

--- frontend/src/app/components/pdf/pdf.component.ts
+++ frontend/src/app/components/pdf/pdf.component.ts
@@ -18,8 +18,8 @@ export class PdfComponent implements OnInit {
 
   fy = ['FY 18-19', 'FY 19-20', 'FY 20-21', 'FY 21-22', ]
 
+  // base schema for column charts
   gbdDiffSchema = {
-
       chart: {
         renderTo: '',
         type: 'column',
@@ -30,7 +30,13 @@ export class PdfComponent implements OnInit {
       },
 
       title: {
-        text: null
+        text: "",
+        align: 'center',
+        verticalAlign: 'bottom',
+        style: {
+          color: '#fff',
+          fontSize: '20px',
+        }
       },
 
       xAxis: {
@@ -48,7 +54,7 @@ export class PdfComponent implements OnInit {
         gridLineWidth:0,
         stackLabels: {
             enabled: true,
-            verticalAlign: 'top'
+            verticalAlign: 'top',
         },
         labels: {
           enabled: false
@@ -64,12 +70,12 @@ export class PdfComponent implements OnInit {
         },
       ],
 
-
       plotOptions: {
         column: {
+            stacking: '',
             colorByPoint: true,
             borderWidth: 0,
-            pointWidth: 30
+            pointWidth: 30,
 
         },
         series: {
@@ -80,57 +86,166 @@ export class PdfComponent implements OnInit {
               color: '#fff',
               fontSize: '20px',
               fontWeight: 100
-            }
+            },
+            allowOverlap: true
           }
         }
       },
 
-
       colors: this.fourColor,
-      legend:{ enabled:false },
+      legend:{
+        enabled: false,
+        verticalAlign: 'top',
+        itemStyle: {
+          color: '#fff',
+          fontSize: '13px',
+          fontWeight: 100
+        }
+      },
 
       credits: {
         enabled: false
       },
+  }
 
-
-
+  // stacking chart's settings
+  private stackingSettings(series: any[], renderTo:string, text: string) {
+    var gbdDiffSchema = JSON.parse(JSON.stringify(this.gbdDiffSchema))
+    gbdDiffSchema.chart.renderTo = renderTo
+    gbdDiffSchema.title.text = text
+    gbdDiffSchema.series = series
+    gbdDiffSchema.chart.width = 500
+    gbdDiffSchema.plotOptions.column.stacking = 'normal'
+    gbdDiffSchema.plotOptions.column.colorByPoint = false
+    gbdDiffSchema.plotOptions.column.pointWidth = 40
+    gbdDiffSchema.plotOptions.series.dataLabels.style.fontSize = '18px'
+    gbdDiffSchema.yAxis.stackLabels.enabled = false,
+    gbdDiffSchema.legend.enabled = true
+    return gbdDiffSchema
   }
 
   constructor() { }
 
   private increaseCompletionRates(): void {
-    this.gbdDiffSchema.chart.renderTo = 'increaseCompletionRates'
-    this.gbdDiffSchema.series[0].data = [82, 78, 88, 92]
-    var chart = new Highcharts.Chart(this.gbdDiffSchema as any)
+    var gbdDiffSchema = JSON.parse(JSON.stringify(this.gbdDiffSchema))
+    // var gbdDiffSchema = Object.assign({}, this.gbdDiffSchema)
+    gbdDiffSchema.chart.renderTo = 'increaseCompletionRates'
+    gbdDiffSchema.title.text = 'Increased Completion Rates'
+    gbdDiffSchema.title.verticalAlign = 'top'
+    gbdDiffSchema.series[0].data = [82, 78, 88, 92]
+    var chart = new Highcharts.Chart(gbdDiffSchema as any)
   }
+
 
   private highCertificationRates(): void {
-    this.gbdDiffSchema.chart.renderTo = 'highCertificationRates'
-    this.gbdDiffSchema.series[0].data = [94, 87, 81, 80]
-    var chart = new Highcharts.Chart(this.gbdDiffSchema as any)
+    var gbdDiffSchema = JSON.parse(JSON.stringify(this.gbdDiffSchema))
+    // var gbdDiffSchema = Object.assign({}, this.gbdDiffSchema)
+    gbdDiffSchema.chart.renderTo = 'highCertificationRates'
+    gbdDiffSchema.title.text = 'High Certification Rates'
+    gbdDiffSchema.title.verticalAlign = 'top'
+    gbdDiffSchema.series[0].data = [94, 87, 81, 80]
+    var chart = new Highcharts.Chart(gbdDiffSchema as any)
   }
+
 
   private highSkellsGain(): void {
-    this.gbdDiffSchema.chart.renderTo = 'highSkellsGain'
-    this.gbdDiffSchema.series[0].data = [67, 57, 51, 48]
-    var chart = new Highcharts.Chart(this.gbdDiffSchema as any)
+    var gbdDiffSchema = JSON.parse(JSON.stringify(this.gbdDiffSchema))
+    gbdDiffSchema.chart.renderTo = 'highSkellsGain'
+    // gbdDiffSchema.title.text = 'High Certification Rates'
+    gbdDiffSchema.series[0].data = [67, 57, 51, 48]
+    var chart = new Highcharts.Chart(gbdDiffSchema as any)
   }
 
+
   private consistentConfidenceGain(): void {
-    this.gbdDiffSchema.chart.renderTo = 'consistentConfidenceGain'
-    this.gbdDiffSchema.series[0].data = [50, 58, 60, 59]
-    var chart = new Highcharts.Chart(this.gbdDiffSchema as any)
+    var gbdDiffSchema = JSON.parse(JSON.stringify(this.gbdDiffSchema))
+    gbdDiffSchema.chart.renderTo = 'consistentConfidenceGain'
+    gbdDiffSchema.series[0].data = [50, 58, 60, 59]
+    var chart = new Highcharts.Chart(gbdDiffSchema as any)
   }
+
+  private persentageOfEnrolmentByRole(): void {
+    let renderTo = 'persentageOfEnrolmentByRole'
+    let text = 'Percentage of Enrolment  by Role'
+    var series = [{
+          name: 'Manager',
+          data: [44, 36, 40, 35],
+          color: '#7f7f7f',
+          dataLabels: {
+            style: {
+              fontSize: "18px",
+              color: "#000",
+              fontWeight: 600
+            },
+          },
+        }, {
+            name: 'Individual Contributor',
+            data: [56, 64, 60, 65],
+            color: '#34834aed'
+        }]
+    var chart = new Highcharts.Chart(this.stackingSettings(series, renderTo, text) as any)
+  }
+
+
+  private persentageOfEnrolmentByExperience(): void {
+    let renderTo = 'persentageOfEnrolmentByExperience'
+    let text = 'Percentage of Enrolment by Experience'
+    var series = [{
+          name: '15 years',
+          data: [3, 16, 20, 21],
+          color: '#ddd64e',
+          dataLabels: {
+            style: {
+              fontSize: "18px",
+              color: "#000",
+              fontWeight: 600
+            },
+          },
+        }, {
+          name: '10-15 years',
+          data: [9, 24, 32, 31],
+          color: '#34834aed'
+        }, {
+          name: '5-10 years',
+          data: [24, 28, 31, 27],
+          color: '#6dbc78'
+        }, {
+          name: '< 5 years',
+          data: [64, 31, 18, 21],
+          color: '#7f7f7f'
+        }]
+    var chart = new Highcharts.Chart(this.stackingSettings(series, renderTo, text) as any)
+  }
+
+  private worckExperiencePieChart(): void {
+      let series = [{
+        name: null,
+        innerSize: '50%',
+        data: [23, 45, 12],
+      }]
+      let gbdDiffSchema = JSON.parse(JSON.stringify(this.gbdDiffSchema))
+      gbdDiffSchema.chart.renderTo = 'worckExperiencePieChart'
+      gbdDiffSchema.chart.type = 'pie'
+      gbdDiffSchema.series = series
+      var chart = new Highcharts.Chart(gbdDiffSchema as any)
+    }
+
 
 
   ngOnInit(): void {
     this.increaseCompletionRates(),
-    this.highCertificationRates(),
+    this.highCertificationRates()
     this.highSkellsGain(),
-    this.consistentConfidenceGain()
+    this.consistentConfidenceGain(),
+    this.persentageOfEnrolmentByRole(),
+    this.persentageOfEnrolmentByExperience(),
+    this.worckExperiencePieChart()
+
   }
-
-
-
 }
+// fourColor = [
+//   '#7f7f7f',
+//   '#595959',
+//   '#6dbc78',
+//   '#34834a',
+// ]
